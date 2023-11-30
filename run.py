eced80db9ba72107115981c148aafe2ab9436ab2
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2023-11-30 10:02:37 +0100

    added three html templates + call these templated through flask to render each on when we clicked on the link related to this template

--- run.py
+++ run.py
@@ -1,13 +1,22 @@
 import os
-from flask import Flask   # Import Flask Class
+from flask import Flask, render_template   # Import Flask Class and render_template function
 
 app = Flask(__name__)    # Creating Instance and store it inside variable called app ====>>>>>>the first argument of the flask class is the name of application module
 
 @app.route("/")    # Decorator app.route  decorator to wrapping functions
 def index():
-    return "Hello, World"
+    return render_template("index.html")
+                        #"Hello, World"     # also you can put html elements here ex: "<h1>Hello, </h1><h2>World</h2>" instead of "Hello, World"
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
